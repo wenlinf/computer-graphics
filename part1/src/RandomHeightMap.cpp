8ed3a3869a45dc0fad52cf1f2c471bfc939bf9ba
Wenlin Fang <fang.wenl@northeastern.edu>
2022-12-07 19:37:09 -0500

    clean up

--- part1/src/RandomHeightMap.cpp
+++ part1/src/RandomHeightMap.cpp
@@ -15,61 +15,9 @@
 #include <algorithm>
 #include <ctime>
 
-template<typename T>
-class Vec2
-{
-public:
-    Vec2() : x(T(0)), y(T(0)) {}
-    Vec2(T xx, T yy) : x(xx), y(yy) {}
-    Vec2 operator * (const T &r) const { return Vec2(x * r, y * r); }
-    Vec2& operator *= (const T &r) { x *= r, y *= r; return *this; }
-    T x, y;
-};
-
-template<typename T>
-class Vec3
-{
-public:
-    Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-    Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-    Vec3 operator * (const T &r) const { return Vec3(x * r, y * r, z * r); }
-    Vec3 operator - (const Vec3 &v) const { return Vec3(x - v.x, y - v.y, z - v.z); }
-    Vec3& operator *= (const T &r) { x *= r, y *= r, z *= r; return *this; }
-    T length2() const { return x * x + y * y + z * z; }
-    Vec3& operator /= (const T &r) { x /= r, y /= r, z /= r; return *this; }
-    Vec3 cross(const Vec3 &v) const
-    {
-        return Vec3(
-                y * v.z - z * v.y,
-                z * v.x - x * v.z,
-                x * v.y - y * v.x
-        );
-    }
-    Vec3& normalize()
-    {
-        T len2 = length2();
-        if (len2 > 0) {
-            T invLen = T(1) / sqrt(len2);
-            x *= invLen, y *= invLen, z *= invLen;
-        }
-        return *this;
-    }
-    friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
-    {
-        os << v.x << ", " << v.y << ", " << v.z;
-        return os;
-    }
-    T x, y, z;
-};
-
-typedef Vec2<float> Vec2f;
-typedef Vec3<float> Vec3f;
-
-template<typename T = float>
 class PerlinNoise
 {
 public:
-
     void PerlinNoise2D(int nWidth, int nHeight, float *fSeed, int nOctaves, float fBias, float *fOutput)
     {
         // Used 1D Perlin Noise
@@ -104,19 +52,9 @@ public:
                 fOutput[y * nWidth + x] = fNoise / fScaleAcc;
 
             }
-
     }
 };
 
-// Noise params
-// the number of levels of detail you want you perlin noise to have.
-//int numLayers = 5;
-//float meshHeight = 32;  // Vertical scaling
-//float noiseScale = 64;  // Horizontal scaling
-////  number that determines how much each octave contributes to the overall shape (adjusts amplitude)
-//float persistence = 0.5;
-////number that determines how much detail is added or removed at each octave (adjusts frequency).
-//float lacunarity = 2;
 int imageHeight = 1024;
 int imageWidth = 1024;
 
@@ -131,17 +69,7 @@ RandomHeightMap::~RandomHeightMap(){
 
 void RandomHeightMap::generateRandomNoiseMap(int octaves) {
     PerlinNoise noise;
-//    Vec3f derivs;
-//    const uint32_t width = 512, height = 512;
     float *noiseMap = new float[imageWidth * imageHeight];
-//
-//    for (uint32_t j = 0; j < imageHeight; ++j) {
-//        for (uint32_t i = 0; i < imageWidth; ++i) {
-//            noiseMap[j * imageWidth + i] = (noise.eval(Vec3f(i, 0, j) * (1 / 64.), derivs) + 1) * 0.5;
-//        }
-//    }
-    // create an array that is filled with random numbers between 0 and 1
-//    srand ( time(NULL) );
     float* fNoiseSeed2D = new float[imageWidth * imageHeight];
     if (octaves > 9) {
         octaves = 9;
@@ -161,7 +89,6 @@ void RandomHeightMap::generateRandomPPM(float *noiseMap) {
     ofs.open("./noise2.ppm", std::ios::out | std::ios::binary);
     ofs << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
     for (unsigned k = 0; k < imageWidth * imageHeight; ++k) {
-//        unsigned char n = static_cast<unsigned char>(noiseMap[k] * 255);
         int n = (int)(noiseMap[k] * 255);
         ofs << n << "\n";
         ofs << n << "\n";
